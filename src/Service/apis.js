da4debda4ef0a6511afa636a2d61284e80e235ec
utkarshg9981 <utkarshg9981@gmail.com>
2025-09-17 15:02:11 +0530

    y

--- src/Service/apis.js
+++ src/Service/apis.js
@@ -2,7 +2,7 @@ const BASE_URL = process.env.REACT_APP_BASE_URL
 
 // AUTH ENDPOINTS
 export const endpoints = {
-  SENDOTP_API: "https://vidyavardani.vercel.app/api/v1/auth/sendotp",
+  SENDOTP_API: BASE_URL + "/auth/sendotp",
   SIGNUP_API: BASE_URL + "/auth/signup",
   LOGIN_API: BASE_URL + "/auth/login",
   RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token",
