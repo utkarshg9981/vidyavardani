0b909d36044f16e51cbeb6385e88a4a5b1b7eef8
utkarshg9981 <utkarshg9981@gmail.com>
2025-10-28 14:35:17 +0530

    logo and cors added

--- Server/Index.js
+++ Server/Index.js
@@ -1,35 +1,30 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
 const app = express();
 
-const userRoutes = require("./Route/User");
-const profileRoutes = require("./Route/Profile");
-const courseRoutes = require("./Route/Course");
-const paymentRoutes = require("./Route/Payment");
-const contactUsRoute = require("./Route/Contact");
-const database = require("./Configuration/Database");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const { cloudinaryConnect } = require("./Configuration/Cloudinary");
-const fileUpload = require("express-fileupload");
-const dotenv = require("dotenv");
+import userRoutes from "./Route/User";
+import profileRoutes from "./Route/Profile";
+import courseRoutes from "./Route/Course";
+import paymentRoutes from "./Route/Payment";
+import contactUsRoute from "./Route/Contact";
+import database from "./Configuration/Database";
+import cookieParser from "cookie-parser";
+import { cloudinaryConnect } from "./Configuration/Cloudinary";
+import fileUpload from "express-fileupload";
+import dotenv from "dotenv";
 
 // Load environment variables first
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-
 database.connect();
- 
+
+app.use(cors()); // enable CORS
+app.options("*", cors()); // allow preflight for all routes
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-	cors({
-		origin: "*",
-		credentials: true,
-	})
-);
 app.use(
 	fileUpload({
 		useTempFiles: true,
@@ -37,9 +32,7 @@ app.use(
 	})
 );
 
-
 cloudinaryConnect();
-
 
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
@@ -47,14 +40,12 @@ app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 app.use("/api/v1/reach", contactUsRoute);
 
-
 app.get("/", (req, res) => {
 	return res.json({
 		success: true,
 		message: "Welcome To Learnify",
 	});
 });
-
 
 app.listen(PORT, () => {
 	console.log(`App is listening at ${PORT}`);
